b46aa65730e91e98a3c6751ce67af53cf591745c
Chema <jomaroru7@gmail.com>
2025-03-13 13:34:51 +0100

    Added styles to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,19 +3,28 @@ import ThemeToggle from "./ThemeToggle"
 import LanguageSwitcher from "./LanguageSwitcher";
 
 function Header() {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
   return (
-    <div className="flex flex-col h-96 bg-secondary text-main">
+    <div className="flex flex-col justify-between min-h-96 bg-secondary text-main">
       <div className="flex flex-row justify-between px-4 py-4">
-        <h1 className="text-2xl">Jomaroru</h1>
+        <h3 className="text-2xl">Jomaroru</h3>
         <ThemeToggle />
-        <LanguageSwitcher/>
+        <LanguageSwitcher />
       </div>
-      <div className="flex flex-row justify-center">
-        <img className="w-32 rounded-full" src="./yo_cafe.png" alt="yo" />
-        <p>
-          {t('presentation')}
-        </p>
+      <div className="flex flex-col items-center lg:flex-row justify-center mb-5 gap-8">
+        <div
+          className="w-70 h-70 rounded-full bg-center bg-cover"
+          style={{ backgroundImage: `url(./yo_cafe.png)` }}
+        ></div>
+        <div className="flex flex-col w-lg gap-4">
+          <h1 className="text-3xl">
+            {t('presentation')}
+          </h1>
+          <p>
+            {t('developer')}
+          </p>
+        </div>
+
       </div>
     </div>
   )
