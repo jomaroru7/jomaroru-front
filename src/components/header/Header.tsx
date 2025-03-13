ed10fc1cdfb596eb818be132164d1a1d5c9ecfe4
Chema <jomaroru7@gmail.com>
2025-03-13 12:27:31 +0100

    Added translations

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,20 @@
+import { useTranslation } from "react-i18next"
 import ThemeToggle from "./ThemeToggle"
+import LanguageSwitcher from "./LanguageSwitcher";
 
 function Header() {
+  const {t} = useTranslation();
   return (
     <div className="flex flex-col h-96 bg-secondary text-main">
       <div className="flex flex-row justify-between px-4 py-4">
         <h1 className="text-2xl">Jomaroru</h1>
         <ThemeToggle />
+        <LanguageSwitcher/>
       </div>
       <div className="flex flex-row justify-center">
         <img className="w-32 rounded-full" src="./yo_cafe.png" alt="yo" />
         <p>
-          Hi, I am José María, Chema for my friends.
+          {t('presentation')}
         </p>
       </div>
     </div>
---
+++ src/components/header/LanguageSwitcher.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+const LanguageSwitcher: React.FC = () => {
+    const { i18n } = useTranslation();
+
+    const toggleLanguage = () => {
+        const newLanguage = i18n.language === 'en' ? 'es' : 'en';
+        i18n.changeLanguage(newLanguage);
+    };
+
+    return (
+        <div
+            className="relative flex items-center border border-gray-300 bg-main rounded-full p-0.5 w-16 h-8 cursor-pointer"
+            onClick={toggleLanguage}
+        >
+            <div
+                className={`absolute w-7 h-7 bg-cover bg-center rounded-full transition-transform duration-300 ease-in-out ${i18n.language === 'en' ? 'translate-x-0' : 'translate-x-7.5'}`}
+                style={{ backgroundImage: `url(${i18n.language === 'en' ? './us.png' : './es.png'})` }}
+            />
+        </div>
+    );
+};
+
+export default LanguageSwitcher;
