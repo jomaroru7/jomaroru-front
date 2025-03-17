ab7f812a2c4a3402bdca746c5442226ed2732c0c
Chema <jomaroru7@gmail.com>
2025-03-17 17:06:05 +0100

    Fixed production bug with separator

--- src/components/Separator.tsx
+++ src/components/Separator.tsx
@@ -6,7 +6,7 @@ type SeparatorProps = {
 const Separator = ({left}:SeparatorProps) => {
 
     return (
-        <div className={`w-full lg:w-5xl flex flex-row${!left && "-reverse"} mx-auto`}>
+        <div className={`w-full lg:w-5xl flex ${left ? "flex-row": "flex-row-reverse"} mx-auto`}>
             <div className="my-4 border-t border-color-primary w-2/3 " />
         </div>
     )
