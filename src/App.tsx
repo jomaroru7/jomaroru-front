7b6c2bf0f6baafa1642fc6447c03b3b37afde443
Chema <jomaroru7@gmail.com>
2025-03-17 12:38:44 +0100

    Changed top menu to be fixed

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from "./components/header/Header"
 function App() {
 
   return (
-    <div className="flex flex-col min-h-screen bg-secondary text-main">
+    <div className="flex flex-col min-h-screen bg-main text-main">
       <Header/>
       <AboutMe/>
     </div>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,13 +6,13 @@ import SocialGrid from "./SocialGrid";
 const Header: React.FC = () => {
   const { t } = useTranslation();
   return (
-    <div className="bg-secondary flex justify-center">
-      <header className="flex flex-col justify-between min-h-96 bg-secondary w-5xl p-4">
-        <div className="flex flex-row justify-between py-4">
-          <h3 className="text-2xl">Jomaroru</h3>
-          <ThemeToggle />
-          <LanguageSwitcher />
-        </div>
+    <div className="flex justify-center">
+      <div className="fixed top-0 flex flex-row justify-between py-4 px-4 w-full lg:w-5xl bg-main">
+        <h3 className="text-2xl">Jomaroru</h3>
+        <ThemeToggle />
+        <LanguageSwitcher />
+      </div>
+      <header className="flex flex-col justify-between min-h-96 w-5xl p-4 pt-20">
         <div className="flex flex-col-reverse items-center lg:flex-row lg:justify-between justify-center mb-5 gap-8">
           <div className="flex flex-col max-w-lg gap-4">
             <h1 className="text-6xl">
