41838487afbe87bf5320ed013254b276ffb12520
Chema <jomaroru7@gmail.com>
2025-03-30 12:11:03 +0200

    Updated version

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Projects from "./components/projects/Projects"
 import Separator from "./components/Separator"
 
 function App() {
-  const version="0.3.5";
+  const version="0.3.6";
   console.log(version);
 
   return (
