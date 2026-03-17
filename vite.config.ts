fafe41cca9d7979bedeafb21d4a304de45bdde1d
Chema <jomaroru7@gmail.com>
2026-03-17 09:05:58 +0100

    Reverted vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jomaroru-front/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss()
